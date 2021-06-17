2d828b0f484156170bb834f2c885045b345e405a
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-06-17 15:33:57 -0700

    sequilizer set up (psql tabesl)

--- server/models/models.js
+++ server/models/models.js
@@ -1,8 +1,7 @@
 const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
 
-const [DataTypes] = require('sequelize')
-
-
+//Modules for Data Structure (Tabes PSQL)
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
@@ -10,4 +9,82 @@ const User = sequelize.define('user', {
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 
 })
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketDevice = sequelize.define('basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define('device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.INTEGER, unique: true, allowNull: false},
+    price: {type: DataTypes.STRING, unique: true, allowNull: false},
+    rating: {type: DataTypes.INTEGER, unique: true, allowNull: false},
+    img: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Brand = sequelize.define('brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+})
+
+const DeviceInfo = sequelize.define('device_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+})
+
+//Connecting Model for .belongsToMany (PSQL)
+const TypeBrand = sequelize.define('type_brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+
+//Data Structure Tabes Relations
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Type)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand })
+Brand.belongsToMany(Type, {through: TypeBrand })
+
+module.exports = {
+    User, Basket, BasketDevice, Device, Type, Brand, Rating, TypeBrand, DeviceInfo
+}
+
+
+
 
