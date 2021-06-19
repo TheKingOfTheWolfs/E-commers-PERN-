39e7233c044e877e294a01e2588d333b95f67af6
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-06-18 17:38:05 -0700

    JWT and auth pack instalment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,26 @@
-const ApiError = require('../error/ApiError')
+const ApiError = require("../error/ApiError");
+const bcrypt = require("../models/models");
+const { User, Basket } = require("../models/models");
 
-class UserController { 
-
-    async registration(req, res)  {
-    
+class UserController {
+  async registration(req, res) {
+    const { email, password, role, next } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Wrong email or password"));
     }
-    async login(req, res)  {
-    
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest("Email is taken"));
     }
-    async check(req, res, next)  {
-     const {id} = req.query
-     if(!id) {
-         return next(ApiError.badRequest('No id'))
-     }
-     res.json(id)
+  }
+  async login(req, res) {}
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest("No id"));
     }
+    res.json(id);
+  }
 }
 
-
-
-module.exports = new UserController()
+module.exports = new UserController();
