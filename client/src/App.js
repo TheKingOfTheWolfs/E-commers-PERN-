68e17c8c94b346130470de0c11cb76a8840d80c7
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-06-20 15:59:08 -0700

    built out initial page structure(fix router bug)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import AppRouter from './components/AppRouter';
 
 
 function App() {
   return (
-    <div>
-     <h1>WORKING</h1>
-    </div>
+    <BrowserRouter>
+    <AppRouter/>
+    </BrowserRouter>
   );
 }
 
