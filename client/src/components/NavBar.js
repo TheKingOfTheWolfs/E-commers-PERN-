4d74e18ba323b794913b83b530d21c34898763be
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-06-21 18:08:16 -0700

    Login/Registration visuals and routs created

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,20 +4,32 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import {NavLink} from 'react-router-dom'
 import { SHOP_ROUTE } from '../utils/const'
+import {Button} from 'react-bootstrap'
+import {observer} from 'mobx-react-lite'
+import Container from 'react-bootstrap/Container'
 
-const NavBar = () => {
+
+
+const NavBar = observer (() => {
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" variant="dark">
-          <NavLink to={SHOP_ROUTE}></NavLink>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Container>
+          <NavLink style={{color:'white'}}to={SHOP_ROUTE}>Stop-By-Shop</NavLink>
+          {user.isAuth ?
+          <Nav className="ml-auto" style={{color:'white'}}>
+            <Button variant={"outline-light"}>Login</Button>
+            <Button variant={"outline-light"} className='ml-2'>Admin</Button>
           </Nav>
+          :
+          <Nav className="ml-auto" style={{color:'white'}}>
+            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Login</Button>
+          </Nav>
+          }
+          </Container>
         </Navbar>
         
     )
-}
+})
 
 export default NavBar
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import {Container, Form, Button, Row} from "react-bootstrap"
+import Card from 'react-bootstrap/Card'
+import { NavLink, useLocation, userLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/const'
 
 function auth() {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
         <div>
-            <h1>AUTH</h1>
+            <Container 
+            className='d-flex justify-content-center alighn-items-center'
+            style={{height: window.innerHeight - 54}}
+            >
+                    <Card style={{width: 600}} className='p-5'>
+                        <h2 className='m-auto'>{isLogin ? 'Login' : 'Register'}</h2>
+                        <Form className='d-flex flex-column'>
+                            <Form.Control
+                            className='mt-2'
+                            placeholder='Enter email...'
+                            />
+                            <Form.Control
+                            className='mt-2'
+                            placeholder='Enter password...'
+                            />
+                            <Row className='d-flex justify-content-between mt-3 pl-3'>
+                                {isLogin ?
+                                <div>Dont have an account? <NavLink to={REGISTRATION_ROUTE}>Register</NavLink>
+                                </div>
+                                :
+                                <div>Have an account? <NavLink to={LOGIN_ROUTE}>Login</NavLink>
+                                </div>
+                               }
+                            <Button 
+                             variant={'outline-success'}
+                             className='align-self-end' 
+                            >
+                              {isLogin ? 'Login' : 'Register'}
+                            </Button>
+                            </Row>
+
+                        </Form>
+                    </Card> 
+            </Container>
             
         </div>
     )
