daaeaf872180e4fc8aeca74008f84dbbe27a124b
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-06-23 23:59:45 -0700

    admin functionality to add device, connect backend

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,34 +1,93 @@
-import React from 'react'
-import Modal from "react-bootstrap/Modal"
-import {Form, Button} from "react-bootstrap"
+import React, { useContext, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { Form, Button, Dropdown, Row, Col } from "react-bootstrap";
+import { Context } from "../..";
 
-const CreateDevice = ({show, onHide}) => {
-    return (
-        <Modal
-        show={show}
-        onHide={onHide}
+const CreateDevice = ({ show, onHide }) => {
+  const { device } = useContext(Context);
+  const [info, setinfo] = useState([]);
+
+  const addInfo = () => {
+    setinfo([...info, { title: '', description: '', number: Date.now() }]);
+  };
+
+  const removeInfo = (number) => {
+    setinfo(info.filter(i => i.number !== number));
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-           Add New Type
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Add Device</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-          <Form>
-              <Form.Control placeholder={"Enter type name"}>
-                 
-              </Form.Control>
-          </Form>
+        <Form>
+          <Dropdown className="mt-2 mb-2">
+            <Dropdown.Toggle>Choose Type</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map((type) => (
+                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown className="mt-2 mb-2">
+            <Dropdown.Toggle>Choose Brand</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map((brand) => (
+                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form.Control placeholder={"Enter device name"} className="mt-3" />
+          <Form.Control
+            placeholder={"Enter price"}
+            className="mt-3"
+            type="number"
+          />
+          <Form.Control className="mt-4 mb-2" type="file" />
+          <Button variant={"outline-dark"} onClick={addInfo}>
+            Add Description
+          </Button>
+          {info.map((i) => (
+            <Row className="mt-4" key={i.number}>
+              <Col md={4}>
+                <Form.Control
+                  placeholder="Enter device name"
+                />
+              </Col>
+              <Col md={4}>
+                <Form.Control
+                  placeholder="Enter device description"
+                />
+              </Col>
+              <Col md={4}>
+                <Button
+                  onClick={() => removeInfo(i.number)}
+                  variant={"outline-danger"}
+                >
+                  Remove
+                </Button>
+              </Col>
+            </Row>
+          ))}
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant='outline-danger' onClick={onHide}>Close</Button>
-        <Button variant='outline-success' onClick={onHide}>Add</Button>
+        <Button variant="outline-danger" onClick={onHide}>
+          Close
+        </Button>
+        <Button variant="outline-success" onClick={onHide}>
+          Add
+        </Button>
       </Modal.Footer>
     </Modal>
-    )
-}
+  );
+};
 
-export default CreateDevice
+export default CreateDevice;
